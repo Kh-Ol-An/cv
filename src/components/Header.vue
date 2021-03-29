f820643e855b6c9e2d620084c92b91a32309dcfd
Oleg <Khristenko>
2021-03-29 17:16:07 +0300

    Configured for use

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Header</h1>
+</template>
+
+<script>
+export default {
+    name: "Header"
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~@/styles/variables.scss";
+h1 {
+    color: $active-color;
+}
+</style>
